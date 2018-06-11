dbe911fa0f5d0d2ad991233576e2b3758fbe77e7
pyh <alanpeng@thetiger.com.cn>
2018-06-11 19:01:11 +0800

    swoole

--- routes/websocket.php
+++ routes/websocket.php
@@ -14,6 +14,7 @@ use SwooleTW\Http\Websocket\Facades\Websocket;
 
 Websocket::on('connect', function ($websocket, Request $request) {
     // called while socket on connect
+    $websocket->emit('message', 'welcome');
 });
 
 Websocket::on('disconnect', function ($websocket) {
